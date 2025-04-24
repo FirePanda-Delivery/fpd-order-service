4d04e960e5c5e32ee26ee74ca70e5a2305be700e
vadim <vadim.belyy.02@mail.ru>
2025-04-24 22:54:30 +0300

    init

---
+++ src/main/java/ru/diplom/fpd/order/FpdOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.diplom.fpd.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpdOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpdOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/diplom/fpd/order/FpdOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diplom.fpd.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpdOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
